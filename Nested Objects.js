b52820238b9c624e2383ac0546fbf82c5c4b4406
AKASH B <147173640+Akash242005@users.noreply.github.com>
2024-12-23 11:33:45 +0530

    Create Nested Objects.js

---
+++ Nested Objects.js
@@ -0,0 +1,13 @@
+const detail = {
+    name : "AKASH",
+    last : "B",
+    age : 20,
+    hobbie : ["Cricket","Coding","Music"],
+    address : {
+        no : "278/M2",
+        street : "Chavadi street",
+        nagar : "Anbu nagar 2",
+        city : "Namakkal"
+    }
+}
+console.log(detail.address.no)
